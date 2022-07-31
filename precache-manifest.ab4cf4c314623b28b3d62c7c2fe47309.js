898d187b9184b98c52887cba679320735993538b
Diego Guterres da Silva <diegoguterresdasilva@hotmail.com>
2022-07-31 16:49:01 -0300

    Updates

---
+++ precache-manifest.ab4cf4c314623b28b3d62c7c2fe47309.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a872b947eb028c9ac904f63f5dbb2a14",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "f7e4d1482479f572cd46",
+    "url": "/portifolio/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "2d51f92433320a712287",
+    "url": "/portifolio/static/css/main.751863ac.chunk.css"
+  },
+  {
+    "revision": "f7e4d1482479f572cd46",
+    "url": "/portifolio/static/js/2.5aba7402.chunk.js"
+  },
+  {
+    "revision": "0297ac78c823ef9b172e38637e06e92e",
+    "url": "/portifolio/static/js/2.5aba7402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d51f92433320a712287",
+    "url": "/portifolio/static/js/main.c19fb8f4.chunk.js"
+  },
+  {
+    "revision": "d87a1418557d4cf2821b",
+    "url": "/portifolio/static/js/runtime-main.f6570ed9.js"
+  }
+]);
