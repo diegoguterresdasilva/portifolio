7bdf5fe4135e1ea761551cf28bd8b9e36356f61f
Diego Guterres da Silva <diegoguterresdasilva@hotmail.com>
2022-07-31 15:14:17 -0300

    inicio

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
+import laravelIcon from "@iconify/icons-logos/laravel";
 import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import typescriptIcon from "@iconify/icons-logos/typescript-icon";
 
 class About extends Component {
   render() {
@@ -31,7 +31,7 @@ class About extends Component {
                     alt="Avatar placeholder"
                   />
                   <Icon
-                    icon={angularIcon}
+                    icon={laravelIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
@@ -39,7 +39,7 @@ class About extends Component {
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
-                    icon={vueIcon}
+                    icon={typescriptIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                 </span>
--- src/components/Header.js
+++ src/components/Header.js
@@ -35,8 +35,51 @@ class Header extends Component {
     }, (props, prevProp) => true);
 
     return (
-      <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
+      <header id="home" style={{ height: window.innerHeight - 340, display: 'block' }}>
+        <div className="col-md-12 pull-right menu">
+          <Switch
+            checked={this.state.checked}
+            onChange={this.onThemeSwitchChange}
+            offColor="#d6d6d6"
+            onColor="#353535"
+            className="react-switch mx-auto"
+            width={90}
+            height={40}
+            uncheckedIcon={
+              <span
+                className="iconify"
+                data-icon="twemoji:owl"
+                data-inline="false"
+                style={{
+                  display: "block",
+                  height: "100%",
+                  fontSize: 25,
+                  textAlign: "end",
+                  marginLeft: "20px",
+                  color: "#353239",
+                }}
+              ></span>
+            }
+            checkedIcon={
+              <span
+                className="iconify"
+                data-icon="noto-v1:sun-with-face"
+                data-inline="false"
+                style={{
+                  display: "block",
+                  height: "100%",
+                  fontSize: 25,
+                  textAlign: "end",
+                  marginLeft: "10px",
+                  color: "#353239",
+                }}
+              ></span>
+            }
+            id="icon-switch"
+          />
+        </div>
         <div className="row aligner" style={{height: '100%'}}>
+        
           <div className="col-md-12">
             <div>
               <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
@@ -47,46 +90,7 @@ class Header extends Component {
               <div className="title-container">
                 <HeaderTitleTypeAnimation />
               </div>
-              <Switch
-                checked={this.state.checked}
-                onChange={this.onThemeSwitchChange}
-                offColor="#baaa80"
-                onColor="#353535"
-                className="react-switch mx-auto"
-                width={90}
-                height={40}
-                uncheckedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="twemoji:owl"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "20px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                checkedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="noto-v1:sun-with-face"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "10px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                id="icon-switch"
-              />
+              
             </div>
           </div>
         </div>
